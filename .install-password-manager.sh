7994b4d5fbc61c19e728cb5f1c82ec40424a6798
Xevion <xevion@xevion.dev>
2024-10-11 02:30:02 -0500

    use --help to ensure exit code is 0, add --no-confirm for rbw install

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -4,14 +4,14 @@ type rbw >/dev/null 2>&1 && exit
 
 install_cargo_binstall() {
     # Test if cargo binstall is installed
-    cargo binstall
+    cargo binstall --help
     if [ $? -eq 0 ]; then
         return
     fi
     curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
     # Test again
-    cargo binstall
+    cargo binstall --help
     if [ $? -ne 0 ]; then
         echo "Failed to install cargo binstall"
         exit 1
@@ -21,7 +21,7 @@ install_cargo_binstall() {
 install_rbw() {
     # Test if rbw is installed
     command -v rbw >/dev/null 2>&1 && return
-    cargo binstall rbw
+    cargo binstall rbw --no-confirm
 
     # Test again
     command -v rbw >/dev/null 2>&1
